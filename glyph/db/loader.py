fb1b16521f3a1556879dbbb162496f4d0b605233
Anish Roy <aroy262692@gmail.com>
2026-05-20 15:56:58 +0530

    add: emojis collection with annotations and collections loader class with emoji loader

---
+++ glyph/db/loader.py
@@ -0,0 +1,17 @@
+
+
+import json
+import os
+from os import path
+
+
+class CollectionLoader:
+    def __init__(self):
+        self.emojis = []
+        self.collection_path = path.join(os.path.dirname(__file__), "collections")
+
+    def LoadEmojis(self):
+        with open(path.join(self.collection_path, "emojis.json"), "r", encoding="utf-8") as f:
+            self.emojis = json.load(f)
+            return self.emojis
+
